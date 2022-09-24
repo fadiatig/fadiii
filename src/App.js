3320b30e3b09014c39a29eb847e003e075040540
atigfadi <fadiatig3@gmail.com>
2022-09-24 09:11:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import fullName from './profile/profile';
+function App() {
+  return (
+    <div className="App">
+      <fullName name="atig fadi" ></fullName>
+    </div>
+  );
+}
+
+export default App;
